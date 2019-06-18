1ea8cdc09b33e4117dbd77d8a0e15fe11321a608
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-06-18 23:55:58 +0800

    Corrected directory name for server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
+app.use(express.static(__dirname + '/dist/bible-reading-tracker-heroku'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bible-reading-tracker-heroku/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
