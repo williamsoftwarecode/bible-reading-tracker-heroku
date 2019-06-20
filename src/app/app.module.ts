e10bb5fcf6ea007c0cb905eee3fd8ca24b2a4601
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-06-20 21:59:09 +0800

    Added Create component for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatOptionModule, 
+  MatSelectModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatCardModule, 
+  MatTableModule, 
+  MatDividerModule, 
+  MatSnackBarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -11,11 +22,26 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ReadingDetailsComponent } from './calendar/reading-details/reading-details.component';
 import { ReadingDetailsEditComponent } from './calendar/reading-details/reading-details-edit/reading-details-edit.component';
 import { ChapterReadService } from './shared/chapter-read.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
-    FormsModule 
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
   declarations: [ 
     AppComponent, 
@@ -24,7 +50,8 @@ import { ChapterReadService } from './shared/chapter-read.service';
     HeaderComponent, 
     DropdownDirective, 
     ReadingDetailsComponent, 
-    ReadingDetailsEditComponent 
+    ReadingDetailsEditComponent, 
+    CreateComponent 
   ],
   providers:    [ ChapterReadService ], 
   bootstrap:    [ AppComponent ]
