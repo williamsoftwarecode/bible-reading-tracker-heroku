eb638848f0bb5577aa66ac16c83d620f91f8e282
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-07-11 19:58:06 +0800

    Add icons for Header buttons (Close #21)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import {DomSanitizer} from '@angular/platform-browser';
+// import {MatIconRegistry} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
   title = 'bible-reading-tracker-heroku';
   loadedFeature: string;
+
+  // constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  //   iconRegistry.addSvgIcon(
+  //     'calendar-icon',
+  //     sanitizer.bypassSecurityTrustResourceUrl('./assets/calendar.svg'));
+  //   iconRegistry.addSvgIcon(
+  //     'view-chart-icon',
+  //     sanitizer.bypassSecurityTrustResourceUrl('./assets/analytics.svg'));
+  //   iconRegistry.addSvgIcon(
+  //     'history-icon',
+  //     sanitizer.bypassSecurityTrustResourceUrl('./assets/list.svg'));
+  // }
 
   ngOnInit() {
     this.loadedFeature = 'calendar';
